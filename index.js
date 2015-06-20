e8e1d542b72bac73555b591c2c00226ee7ff38a9
Christoph Hoffmann <Christoph.Hoffmann@adesso.de>
2015-06-20 14:45:24 +0200

    restructure code

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 "use strict";
 
 var program = require('commander');
-var init = require('./lib/init-db');
-var start = require('./lib/actions/start');
+var init = require('./lib/util/init-db');
 var edit = require('./lib/actions/edit');
+var end = require('./lib/actions/end');
+var create = require('./lib/actions/create');
+var findCurrent = require('./lib/actions/findCurrent');
+var moment = require('moment');
+var a = require('./lib/util/arguments');
+var async = require('async');
+
+function parseTime(dateTime) {
+    return moment(dateTime, 'HH:mm');
+}
+
+function parseDate(dateString) {
+    return moment(dateString, 'DD.MM.YY');
+}
 
 program
     .version('1.0.0');
@@ -12,15 +25,24 @@ program
     .command('start [task]')
     .alias('s')
     .description('starts a new task')
-    .option("-t, --time <time>", "sets the start time (default: now)")
-    .option("-d, --date <date>", "sets the start date (default: today)")
+    .option("-t, --time <time>", "sets the start time (default: now)", parseTime, moment())
+    .option("-d, --date <date>", "sets the start date (default: today)", parseDate, moment())
     .option("-n, --note <note>", "personal notes")
     .option("-p, --project <project>", "project for task")
     .option("--db <db>", "database connection")
     .action(function (task, options) {
         options.task = task;
-        var db = init(options);
-        start(db, options);
+        async.waterfall([
+            a.bind(this, options),
+            findCurrent,
+            end,
+            create,
+            edit
+        ], function (err, args) {
+            if (err) console.log(err);
+            else
+                console.log(args.task);
+        });
     });
 
 program
@@ -34,13 +56,15 @@ program
     .option("-p, --project <project>", "project for task")
     .option("--db <db>", "database connection")
     .action(function (task, options) {
-        console.log("task:" ,task);
-        var db = init(options);
-        db.findOne({ end: { $exists: false } }, function(err, obj){
-            if(err) {
-                console.log("could not find object to edit");
-            }
-            edit(db, obj._id, options);
+        options.task = task;
+        async.waterfall([
+            a.bind(this, options),
+            findCurrent,
+            edit
+        ], function (err, args) {
+            if (err) console.log(err);
+            else
+                console.log(args.task);
         });
     });
 
@@ -56,6 +80,17 @@ program
     .option("--db <db>", "database connection")
     .action(function (task, options) {
         "use strict";
+        options.task = task;
+        async.waterfall([
+            a.bind(this, options),
+            findCurrent,
+            edit,
+            end
+        ], function (err, args) {
+            if (err) console.log(err);
+            else
+                console.log(args.task);
+        });
     });
 
 program
@@ -63,7 +98,37 @@ program
     .alias('r')
     .description('returns to the previous active task')
     .action(function (options) {
-        
+        async.waterfall([
+                a.bind(this, options),
+                function (args, done) {
+                    var db = args.db;
+                    db.find({end: {$exists: true}})
+                        .sort({start: -1})
+                        .limit(1).
+                        exec(function (err, tasks) {
+                            if (tasks.length !== 1)
+                                return done("did not found task to return to");
+
+                            var task = tasks[0];
+                            args.rerun = task;
+                            done(null, task);
+                        })
+                },
+                findCurrent,
+                end,
+                create,
+                //todo set edit options from rerunned task
+                function (args, done){
+                    args.blub = args.rerun;
+                    done(null, args)
+                },
+                edit
+            ], function (err, task) {
+                if (err) console.log(err);
+                else
+                    console.log(task);
+            }
+        )
     });
 
 program
@@ -73,8 +138,8 @@ program
         "use strict";
 
         var db = init(options);
-        db.find({}, function (err, objs){
-            if(err) {
+        db.find({}, function (err, objs) {
+            if (err) {
                 console.log("could not load tasks");
                 return;
             }
