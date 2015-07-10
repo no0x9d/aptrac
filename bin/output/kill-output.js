4734f4e31cdc1eec6b08a965bf11c3bbb2daf3a3
Christoph Hoffmann <christoph.hoffmann@nestedloop.de>
2015-07-10 23:58:43 +0200

    refactor kill command to be testable and added test

---
+++ bin/output/kill-output.js
@@ -0,0 +1,5 @@
+module.exports = function (err, context, numRemoved) {
+    if (err) console.log(err);
+    if (numRemoved === 1)
+        console.log("task with #%s removed", context.options.id)
+}
---
+++ test/integration/kill.test.js
@@ -0,0 +1,34 @@
+var expect = require('chai').expect;
+var Datastore = require('nedb');
+var moment = require('moment');
+
+var aptrac = require('../../lib/aptrac');
+
+describe('kill command', function () {
+   it('should delete a task is id is a single vale', function (done) {
+       var id = 123;
+       var now = moment();
+       var db = new Datastore();
+       db.insert({_id: id, start: now.toDate()}, function (err, task) {
+           if (err) done(err);
+
+           expect(task.start).to.equal(now.toDate());
+           expect(task._id).to.equal(id);
+
+           var context = {
+               isContext: true,
+               options: {
+                   db: null,
+                   id: id
+               },
+               db: db
+           };
+
+           aptrac.kill(context, function (err, context, numRemoved) {
+               if(err) done(err);
+               expect(numRemoved).to.equal(1);
+               done(err)
+           })
+       })
+   })
+});
