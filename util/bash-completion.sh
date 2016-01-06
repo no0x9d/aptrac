3147b220fb3c4a921fe8646ab4dd27f2645812f9
Christoph Hoffmann <christoph.hoffmann@nestedloop.de>
2016-01-06 23:49:31 +0100

    Implementing command for dynamic bash completion, but the bash script is not finished yet

---
+++ util/bash-completion.sh
@@ -0,0 +1,14 @@
+_aptrac()
+{
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    opts= $(for x in `aptrac completion --cmd`; do echo ${x} ; done )
+
+    if [[ ${cur} == -* ]] ; then
+        COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+        return 0
+    fi
+}
+complete -F _aptrac aptrac
